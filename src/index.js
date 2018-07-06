e5bf9cbded40d38958c4f36dbc84a1ef1047032e
terry623 <terry92516@gmail.com>
2018-07-06 14:36:17 +0800

    vote but no feedback

--- src/index.js
+++ src/index.js
@@ -5,14 +5,30 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
+import { ApolloLink } from 'apollo-client-preset';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  const authorizationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader,
+    },
+  });
+  return forward(operation);
+});
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache(),
 });
 
